83b8aa2b5e71cb42f78a144f2775413bbdbb54d1
Rodrigo Queipo <rodrigoq@gmail.com>
2020-05-24 23:06:03 -0300

    Saco distancia.

--- frontend/src/backoffice/views/Metric/CalculatedMetricWizard/StepDistanceOutput.vue
+++ frontend/src/backoffice/views/Metric/CalculatedMetricWizard/StepDistanceOutput.vue
@@ -5,10 +5,8 @@
 		<div>Guardar por cada coincidencia:</div>
 
 		<md-switch class="md-primary" :disabled="!canEdit" v-model="newMetric.Output.HasDescription">Descripción</md-switch>
-		<md-switch class="md-primary" :disabled="!canEdit" v-model="newMetric.Output.HasDistance">Distancia (km)</md-switch>
 		<md-switch class="md-primary" :disabled="!canEdit" v-model="newMetric.Output.HasValue">Valor</md-switch>
 		<md-switch class="md-primary" :disabled="!canEdit" v-model="newMetric.Output.HasCoords">Coordenada</md-switch>
-		<!-- <md&#45;switch v&#45;if="newMetric.SelectedVariable.HasTotals" class="md&#45;primary" :disabled="!canEdit" v&#45;model="newMetric.Output.HasNormalizationValue">Valor de normalización</md&#45;switch> -->
 
 		<div>Limitar coincidencias:</div>
 		<div class="md-layout">
@@ -20,9 +18,6 @@
 					:maxlength="3" type="number" v-model="newMetric.Output.MaxDistance"></mp-simple-text>
 				Kms.
 			</div>
-			<!-- <div class="md&#45;layout&#45;item md&#45;size&#45;40 md&#45;small&#45;size&#45;100"> -->
-			<!-- 	<md&#45;switch class="md&#45;primary" :disabled="!canEdit" v&#45;model="newMetric.Output.InSameProvince">Coincidencias en misma provincia</md&#45;switch> -->
-			<!-- </div> -->
 		</div>
 	</div>
 </template>
