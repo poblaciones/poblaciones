eb5bcd57ad2c2cf11e12912dfaf71ce2151c97e8
Pablo De Grande <pablodg@gmail.com>
2020-07-20 16:17:22 -0300

    script renumerado

--- startup/scripts/038-extra- color primario institucion.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `draft_institution` ADD `ins_color` char(6) NULL COMMENT 'Color primario institucional' AFTER `ins_watermark_id`;
-
-ALTER TABLE `institution` ADD `ins_color` char(6) NULL COMMENT 'Color primario institucional' AFTER `ins_watermark_id`;
-
-UPDATE version SET ver_value = '038' WHERE ver_name = 'DB';
