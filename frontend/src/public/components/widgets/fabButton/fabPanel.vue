1f2db75c289729de988ec2baf7a7c15abe733f1f
Rodrigo Queipo <rodrigoq@gmail.com>
2021-01-11 20:46:19 -0300

    Mejoras a fabPanel.vue en especial scrolling.

--- frontend/src/public/components/widgets/fabButton/fabPanel.vue
+++ frontend/src/public/components/widgets/fabButton/fabPanel.vue
@@ -1,12 +1,13 @@
 <template>
 	<div v-show="visible" :class="[ 'fab-panel', outerBorderRadiusClass ]"
-		 :style="{ 'background-color': bgColor, 'max-width': width + 'px', 'width': getWidth(), 'max-height': maxHeight + 'px' }" ref="fabPanel">
+		:style="{ 'background-color': bgColor, 'max-width': width + 'px', 'width': getWidth }" ref="fabPanel">
 		<div class="fab-triangle" :style="{ 'border-right-color': bgColor }"></div>
-		<div v-if="showScrollButtons" ref="scrollUp" class="fab-scroll-button top-radius" :style="style"
-			@click="scrollUp" @mouseenter="scrollUpStart" @mouseleave="scrollUpStop" v-ripple="rippleEffect">
+		<div v-if="showScrollButtons" ref="scrollUp" class="fab-scroll-button-disabled top-radius" :style="style"
+			@click="scrollUp" @mouseenter="scrollUpStart" @mouseleave="scrollUpStop">
 			<i :class="[ actionIconSize, 'material-icons', 'no-highlight', 'fab-icon-offset' ]">arrow_drop_up</i>
 		</div>
-		<div :class="[ 'fab-panel-overflow', scrollBarClass, borderRadiusClass ]" ref="panelScroll" :style="scrollStyle" :id="items.Name">
+		<div :class="[ 'fab-panel-overflow', scrollBarClass, borderRadiusClass ]" ref="panelScroll"
+			:style="scrollStyle" @wheel="wheel($event)" @scroll="scrolled()">
 			<ul class="fab-panel-list">
 				<li :class="[ 'fab-panel-item', ellipsisClass ]" v-for="item in items" :key="item.Id" :style="style" @click="select(item)"
 					v-tooltip="{ content: item.Name, placement: 'top', classes: 'fab-tooltip', trigger: 'manual' }"
@@ -17,15 +18,13 @@
 			</ul>
 		</div>
 		<div v-if="showScrollButtons" ref="scrollDown" class="fab-scroll-button bottom-radius"
-			:style="style" @click="scrollDown" @mouseenter="scrollDownStart" @mouseleave="scrollDownStop" v-ripple="rippleEffect">
+			:style="style" @click="scrollDown" @mouseenter="scrollDownStart" @mouseleave="scrollDownStop">
 			<i :class="[ actionIconSize, 'material-icons', 'no-highlight', 'fab-icon-offset' ]">arrow_drop_down</i>
 		</div>
 	</div>
 </template>
 
-
 <script>
-
 import {VTooltip} from 'v-tooltip';
 import Ripple from 'vue-ripple-directive';
 
@@ -37,6 +36,8 @@ export default {
 	},
 	data() {
 		return {
+			maxMaxHeight: 230,
+			adjust: 0,
 			visible: false,
 			scrollInterval: null,
 			style: {
@@ -112,19 +113,6 @@ export default {
 		},
 	},
 	computed: {
-		maxHeight() {
-			var height = (this.scrollAt - 1) * this.itemHeight;
-			if(this.scrollButtons) {
-				height += 2 * this.scrollButtonHeight;
-			}
-			return height;
-		},
-		itemHeight() {
-			if(this.$refs.liItems && this.$refs.liItems.length > 0) {
-				return this.$refs.liItems[0].offsetHeight;
-			}
-			return 30;
-		},
 		ellipsisClass() {
 			if(this.ellipsis) {
 				return 'overflow-ellipsis';
@@ -161,8 +149,15 @@ export default {
 			if(this.rippleColor == 'light') {
 				return 'rgba(255, 255, 255, 0.35)';
 			}
-			return '';
+			return false;
 		},
+	},
+	updated() {
+		var maxHeight = this.visibleMaxHeight();
+		if(this.scrollButtons) {
+			maxHeight += 2 * this.scrollButtonHeight;
+		}
+		this.$refs.fabPanel.style.maxHeight = maxHeight + 'px';
 	},
 	methods: {
 		show() {
@@ -186,6 +181,21 @@ export default {
 				return 'unset';
 			}
 		},
+		visibleMaxHeight() {
+			if(this.$refs.liItems && this.$refs.liItems.length > 0) {
+				var total = this.maxMaxHeight + 1;
+				this.adjust = 1;
+				while(total > this.maxMaxHeight) {
+					total = 0;
+					this.adjust--;
+					for (var i = 0; i < Math.min(this.scrollAt + this.adjust - 1, this.$refs.liItems.length); i++) {
+						total += this.$refs.liItems[i].scrollHeight;
+					}
+				}
+				return total;
+			}
+			return this.maxMaxHeight;
+		},
 		scrollUpStart() {
 			if(this.scrollMode == 'click') {
 				return;
@@ -208,16 +218,96 @@ export default {
 				this.scrollDown();
 			}, this.scrollTime);
 		},
+		visiblePixelsItemTop(top, height, itemHeight) {
+			if(top < itemHeight + height
+				&& top > height) {
+				return top - height;
+			}
+			return 0;
+		},
+		prevItemHeight() {
+			const items = this.$refs.liItems;
+			if(items && items.length > 0) {
+				const el = this.$refs.panelScroll;
+				var height = 0;
+				for (var i = 0; i < items.length; i++) {
+					const delta = this.visiblePixelsItemTop(el.scrollTop, height, items[i].scrollHeight);
+					if(delta > 0) {
+						return delta;
+					}
+					height += items[i].scrollHeight;
+					if(el.scrollTop == height) {
+						return items[i].scrollHeight;
+					}
+				};
+				return items[0].scrollHeight;
+			}
+			return 36;
+		},
+		nextItemHeight() {
+			const items = this.$refs.liItems;
+			if(items && items.length > 0) {
+				const el = this.$refs.panelScroll;
+				var height = 0;
+				var text = "";
+				for (var i = 0; i < items.length; i++) {
+					var delta = this.visiblePixelsItemTop(el.scrollTop, height, items[i].scrollHeight);
+					height += items[i].scrollHeight;
+					text = items[i].innerText.trim();
+					if(delta > 0 || el.scrollTop == 0 || el.scrollTop == height) {
+						if(delta > 0) {
+							delta = items[i].scrollHeight - delta;
+						}
+						const next = i + this.scrollAt + this.adjust;
+						for (var j = i + 1; j < next; j++) {
+							delta += items[j].scrollHeight;
+						}
+						if(delta > el.offsetHeight) {
+							return delta - el.offsetHeight;
+						}
+						return items[next].scrollHeight;
+					}
+				};
+				return items[0].scrollHeight;
+			}
+			return 36;
+		},
 		scrollUp() {
-			this.$refs.panelScroll.scroll(0, this.$refs.panelScroll.scrollTop - this.itemHeight);
+			const el = this.$refs.panelScroll;
+			if(el.scrollTop == 0) {
+				return;
+			}
+			el.scroll(0, el.scrollTop - this.prevItemHeight());
 		},
 		scrollDown() {
-			var el = this.$refs.panelScroll;
+			const el = this.$refs.panelScroll;
 			if(el.scrollTop == el.scrollHeight - el.offsetHeight) {
 				return;
 			}
-			const offset = Math.min(this.itemHeight, el.scrollHeight - el.offsetHeight - el.scrollTop);
-			el.scroll(0, el.scrollTop + offset);
+			el.scroll(0, el.scrollTop + this.nextItemHeight());
+		},
+		wheel(e) {
+			e.preventDefault();
+			if(e.deltaY > 0) {
+				this.scrollDown();
+			} else {
+				this.scrollUp();
+			}
+		},
+		scrolled() {
+			const el = this.$refs.panelScroll;
+			if(el) {
+				if(el.scrollTop == 0) {
+					this.$refs.scrollUp.classList.replace('fab-scroll-button', 'fab-scroll-button-disabled');
+				} else {
+					this.$refs.scrollUp.classList.replace('fab-scroll-button-disabled', 'fab-scroll-button');
+				}
+				if(el.scrollTop == el.scrollHeight - el.offsetHeight) {
+					this.$refs.scrollDown.classList.replace('fab-scroll-button', 'fab-scroll-button-disabled');
+				} else {
+					this.$refs.scrollDown.classList.replace('fab-scroll-button-disabled', 'fab-scroll-button');
+				}
+			}
 		},
 		showTooltip(el) {
 			if(this.$isMobile()) {
@@ -241,13 +331,13 @@ export default {
 
 <style>
 .no-scroll-bar {
-	 overflow-y: scroll;
-	 scrollbar-width: none; /* Firefox */
-	 -ms-overflow-style: none;  /* Internet Explorer 10+ */
+	overflow-y: scroll;
+	scrollbar-width: none; /* Firefox */
+	-ms-overflow-style: none; /* Internet Explorer 10+ */
 }
 .no-scroll-bar::-webkit-scrollbar { /* WebKit */
-	 width: 0;
-	 height: 0;
+	width: 0;
+	height: 0;
 }
 
 .overflow-ellipsis {
@@ -281,15 +371,15 @@ export default {
 	scrollbar-color: var(--scrollColor) var(--scrollBgColor);
 }
 .fab-scroll-bar::-webkit-scrollbar {
-  width: 11px;
+	width: 11px;
 }
 .fab-scroll-bar::-webkit-scrollbar-track {
-  background: var(--scrollBgColor);
+	background: var(--scrollBgColor);
 }
 .fab-scroll-bar::-webkit-scrollbar-thumb {
-  background-color: var(--scrollColor);
-  border-radius: 6px;
-  border: 3px solid var(--scrollBgColor);
+	background-color: var(--scrollColor);
+	border-radius: 6px;
+	border: 3px solid var(--scrollBgColor);
 }
 
 .fab-panel-list {
@@ -333,6 +423,13 @@ export default {
 
 .fab-scroll-button:hover {
 	background-color: var(--hover);
+}
+
+.fab-scroll-button-disabled {
+	text-align: center;
+	color: #66615b;
+	cursor: pointer;
+	height: var(--height);
 }
 
 .no-radius {
